203c83c44380edfa840ee97790612495695b3657
LeviSmithCodes <eyevel@gmail.com>
2019-12-09 09:37:19 -0700

    Changed time api to https

--- app/services/time-service.js
+++ app/services/time-service.js
@@ -2,7 +2,7 @@ import store from "../store.js";
 
 // @ts-ignore
 const _timeApi = axios.create({
-    baseURL: "http://worldtimeapi.org/api/ip",
+    baseURL: "https://worldtimeapi.org/api/ip",
     timeout: 8000
 });
 class TimeService {
